0b6b4c8e258514b31483746709882ff874a4ff68
mohanpillimitla <mohanpillimitla143@gmail.com>
2020-05-25 15:02:43 +0530

    initial

--- angularblog/settings.py
+++ angularblog/settings.py
@@ -79,8 +79,23 @@ WSGI_APPLICATION = 'angularblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+      'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME':'stack',
+        'USER':'mohan',
+        'PASSWORD':'Mohan@1210',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
+
+
+
+
+
+
+
+        
     }
 }
 
